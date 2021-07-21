1782741f1070ba59c09b41360e29359769b8d60a
paperwiz-git <hancdh@gmail.com>
2021-07-21 11:47:48 +0900

    Object

--- class.js
+++ class.js
@@ -179,3 +179,31 @@ function makeObj(key, val) {
 const obj = makeObj("나이", 33);
 
 console.log(obj);
+
+const user = {
+    name: "Mike",
+    age: 30,
+};
+
+// Object.assign({초기값}, 복사할 객체); 객체를 복사할때 사용하는 명령어
+const user2 = Object.assign({}, user); 
+
+user2.name = "ellie";
+console.log(user);
+console.log(user2);
+
+// Object.keys() 키 배열로 반환
+const result = Object.keys(user);
+console.log(result);
+
+// Object.values() 값 배열로 반환
+const result2 = Object.values(user);
+console.log(result2);
+
+// Object.entries() 키와 값을 배열로 반환
+const result3 = Object.entries(user);
+console.log(result3);
+
+// Object.fromEntries() 키와 값 배열을 객체로 반환
+const result4 = Object.fromEntries(result3);
+console.log(result4);
