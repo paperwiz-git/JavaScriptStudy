d5f63dd38a2a57dd278475d9da1eb0dc1146e66a
paperwiz-git <hancdh@gmail.com>
2021-07-21 19:14:19 +0900

    list

--- class.js
+++ class.js
@@ -207,3 +207,52 @@ console.log(result3);
 // Object.fromEntries() 키와 값 배열을 객체로 반환
 const result4 = Object.fromEntries(result3);
 console.log(result4);
+
+let list = [
+        "01. 들어가며",
+        "02. JS의 역사",
+        "03. 자료형",
+        "04. 함수",
+        "05. 배열",
+];
+
+let newList = [];
+for(let i = 0; i < list.length; i++) {
+    newList.push(list[i].slice(4));
+}
+
+console.log(newList);
+
+// 금칙어 : 콜라
+// includes
+// 문자가 있으면 true
+// 없으면 false 반환
+
+function hasCola(str) {
+    if(str.indexOf('콜라') > -1) {
+        console.log('금칙어가 있습니다.');
+    } else {
+        console.log('통과');
+    }
+};
+
+hasCola("와 사이다가 짱이야!"); // -1
+hasCola("무슨소리, 콜라가 짱이야!");
+hasCola("콜라");   // 0
+
+function hasCola(str) {
+    if(str.includes('콜라')) {
+        console.log('금칙어가 있습니다.');
+    } else {
+        console.log('통과');
+    }
+};
+
+hasCola("와 사이다가 짱이야!"); // -1
+hasCola("무슨소리, 콜라가 짱이야!");
+hasCola("콜라");   // 0
+
+// forEach
+let arr = ["Mike", "Tom", "Jane"];
+
+arr.forEach((name, index, array) => console.log(name, index, array));
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,27 +1,3 @@
-const user = {
-    name: "Mike",
-    age: 30,
-};
+let arr = ["Mike", "Tom", "Jane"];
 
-// Object.assign({초기값}, 복사할 객체); 객체를 복사할때 사용하는 명령어
-const user2 = Object.assign({}, user); 
-
-user2.name = "ellie";
-console.log(user);
-console.log(user2);
-
-// Object.keys() 키 출력
-const result = Object.keys(user);
-console.log(result);
-
-// Object.values() 값 출력
-const result2 = Object.values(user);
-console.log(result2);
-
-// Object.entries() 키와 값을 배열로 반환
-const result3 = Object.entries(user);
-console.log(result3);
-
-// Object.fronEntries() 키와 값 배열을 객체로 반환
-const result4 = Object.fromEntries(result3);
-console.log(result4);
+arr.forEach((name, index, array) => console.log(name, index, array));
